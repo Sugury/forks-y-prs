f02135da2321c94211d1a6b06e6359b7f2c7f863
unknown <melzigaston@gmail.com>
2024-08-10 20:22:53 -0300

    Modifique el metodo addProducts de class User

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,30 @@ class User {
   name: string;
   products: Product[] = [];
   addProduct(newProduct: Product) {
+
     this.products.push(newProduct);
   }
-  addProducts(newProducts: Product[]) {
+ // ...newProduct:Product[] = es un parametro rest, permite a addProducts aceptar un numero ilimitado de argumentos guardandolos en un array(en este caso de tipo Product)
+ //> Se evita usar un array [ ] en la llamada a la funcion para ingresarle varios parametros a la funcion
+  addProducts(...newProduct:Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products.push(...newProduct);
     // pista: push no suma muchos items (agrega de a uno)
   }
+  
 }
 
 export { User, Product };
+/*
+function main () {
+  const nuevoUsuario = new User ("Pedro");
+  const celular = new Product("Nokia 1100",2000);
+  const notebook = new Product ("Notebook ASUS",700000);
+  const osoPeluche = new Product ("Oso peluche blanco",8000);
+ // nuevoUsuario.addProduct(nuevoProducto);
+ // nuevoUsuario.addProducts(celular,notebook,osoPeluche);
+
+  //console.log(nuevoUsuario);
+}
+main();
+*/
